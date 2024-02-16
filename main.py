1f89a396462f90721b97c9ce3d198212053e1562
Beau Clark <beau28713@gmail.com>
2024-02-16 09:15:18 -0500

    replaced if-else with dic

--- main.py
+++ main.py
@@ -2,23 +2,20 @@ import typer
 import read_json
 
 
-def main(username: str = "" , tag: str = ""):
-    if tag == "followers":
-        read_json.get_followers(username, tag)
-        print("followers has been written to the file")
+def main(username: str = "", tag: str = ""):
 
-    elif tag == "following":
-        read_json.get_following(username, tag)
-        print("following has been written to the file")
+    tag_dic = {
+        "followers": read_json.get_followers,
+        "following": read_json.get_following,
+        "not_following_me": read_json.not_following_me,
+        "following_me": read_json.following_me,
+    }
 
-    elif tag == "not_following_me":
-        read_json.not_following_me()
-
-    elif tag == "following_me":
-        read_json.following_me()
-
-    else:
-        print("Invalid tag")
+    (
+        tag_dic.get(tag, "invalid tag")(username, tag)
+        if tag == "followers" or tag == "following"
+        else tag_dic.get(tag, "invalid tag")()
+    )
 
 
 if __name__ == "__main__":
